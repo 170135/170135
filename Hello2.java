00a58921797b3a17c6c2bb8522f11a59750a0e62
170135 <34013932+170135@users.noreply.github.com>
2017-11-27 12:22:53 +0900

    Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Hello2 {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Hello");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(700,300,500,500);
+		frame.setVisible(true);
+	}
+}
